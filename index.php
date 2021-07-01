b53d8cae312d7123d2fb87d016803cb169607ba8
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-01 18:35:14 +0200

    Al posto dei var dump stampo a schermo con foreach

--- index.php
+++ index.php
@@ -19,10 +19,35 @@ class Movie
     }
 }
 
-$matrix = new Movie('Matrix', 'Azione', 2.15);
+$film = [
+    $matrix = new Movie('Matrix', 'Azione', 2.15),
+    $it = new Movie('It', 'Horror', 1.52),
+    $titanic = new Movie('Titanic', 'Drammatico', 2.30),
+    $as = new Movie('American Sniper', 'Azione', 1.42)
+];
 
-$it = new Movie('It', 'Horror', 1.52);
+/* var_dump($matrix, $it);
 
-var_dump($matrix, $it);
+var_dump($matrix->getTitle(), $it->getTitle()); */
 
-var_dump($matrix->getTitle(), $it->getTitle());
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+
+<body>
+    <?php foreach ($film as $item) : ?>
+    <h1><?php echo $item->getTitle(); ?></h1>
+    <p>Genere: <?php echo $item->genere; ?></p>
+    <p>Durata: <?php echo $item->durata; ?></p>
+    <?php endforeach ?>
+</body>
+
+</html>
